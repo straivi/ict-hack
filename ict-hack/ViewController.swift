c419e018d01a197fc1c75c89a44b1b092a6dfa51
straivi <bme15122000@gmail.com>
2021-05-07 21:20:39 +0300

    init project

---
+++ ict-hack/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ict-hack
+//
+//  Created by  Matvey on 07.05.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
